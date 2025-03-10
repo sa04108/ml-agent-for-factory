bc381a0fbd58980b3e36d36350572f0410c349a2
전현배 <hyunbae204@daimresearch.com>
2025-03-10 10:58:44 +0900

    batch 파일 수정
    - batch 파일을 리포지토리로 옮김에 따라 yaml파일을 상대경로로 찾도록 변경

--- batch/mlagent_run.bat
+++ batch/mlagent_run.bat
@@ -1,9 +1,8 @@
 @echo Activate ml-agent for MAFF
-@echo config path: %userprofile%\ml-agent-for-factory\config\matt_vehicle.yaml
+@echo config path: ..\config\matt_vehicle.yaml
 @echo off
 
-cd %userprofile%
 call conda activate mlagents
-mlagents-learn %userprofile%\ml-agent-for-factory\config\matt_vehicle.yaml --run-id=Vehicle --resume
+cd .. && mlagents-learn config\matt_vehicle.yaml --run-id=Vehicle --resume
 
 pause
